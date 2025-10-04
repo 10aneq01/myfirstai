ab65a6ae776880ac388363537f1ce678da61c57f
Mohamed Aneq <mdaneq006@gmail.com>
2025-10-04 12:45:14 +0530

    Add files via upload

---
+++ webbot.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+from google import genai
+chitti =genai.Client(api_key="AIzaSyDLit1WmoKcR4KdLCz9yk6-hF5L9PrWz_Q")
+st.title("my own chatbot")
+question = st.text_input("ask antything")
+
+if st.button("send"):
+    response = chiti.models.generate_content
+    (
+        model == "gemini-2.5-flash",
+        contents == question
+        )
+st.write(response.text)
+    
